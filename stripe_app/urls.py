ea6d18e09de9d43093045cac17701e69479a97b4
Sergey <kapetanvodichka@gmail.com>
2024-03-01 18:42:54 +0500

    Comments for urls

--- stripe_app/urls.py
+++ stripe_app/urls.py
@@ -7,11 +7,14 @@ from stripe_app.views import get_stripe_session, get_item_page, create_order, ge
 app_name = StripeAppConfig.name
 
 urlpatterns = [
+    # Получение StripeSession и осуществление CheckoutSession для Item
     path('buy/<int:item_id>/', get_stripe_session, name='get_session'),
     path('item/<int:item_id>/', get_item_page, name='item_page'),
 
+    # Создание Order путём сбора корзины из существующих Item
     path('create_order/', create_order, name='create_order'),
 
+    # Получение StripeSession и осуществление CheckoutSession для Order
     path('order/<int:order_id>/', get_order_page, name='order_page'),
     path('buy_order/<int:order_id>/', get_stripe_session_for_order, name='get_session_for_order'),
 ]
